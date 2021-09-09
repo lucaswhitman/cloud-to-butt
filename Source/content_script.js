28977b7bbe74417c9790cc74c262aa0162e34e3b
Lucas Whitman <lucasmichaelwhitman@gmail.com>
2021-09-09 11:49:02 -0600

    pants

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,27 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bA Mask\b/g, "Pants");
+	v = v.replace(/\bA mask\b/g, "Pants");
+	v = v.replace(/\ba Mask\b/g, "Pants");
+	v = v.replace(/\ba mask\b/g, "pants");
+
+  // this one has to come second, so it doesn't block the first
+  v = v.replace(/\bMask\b/g, "Pants");
+	v = v.replace(/\bmask\b/g, "pants");
+
+  v = v.replace(/\bMasks\b/g, "Pants");
+	v = v.replace(/\bmasks\b/g, "pants");
+
+  v = v.replace(/\bAnti-Mask\b/g, "Anti-Pants");
+	v = v.replace(/\bAnti-mask\b/g, "Anti-pants");
+	v = v.replace(/\banti-Mask\b/g, "anti-Pants");
+	v = v.replace(/\banti-mask\b/g, "anti-pants");
+
+  v = v.replace(/\bMask Mandate\b/g, "Pants Mandate");
+	v = v.replace(/\bMask mandate\b/g, "Pants mandate");
+	v = v.replace(/\bmask Mandate\b/g, "pants Mandate");
+	v = v.replace(/\bmask mandate\b/g, "pants mandate");
 	
 	textNode.nodeValue = v;
 }
